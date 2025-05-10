21f3a2ceb35a798efc7faf8741ac8fe2b8457a62
simplyObare <josephatowiny@gmail.com>
2025-05-11 01:17:25 +0300

    adding multiple themes

--- react-tailwind-daisyUI/src/components/Navbar.jsx
+++ react-tailwind-daisyUI/src/components/Navbar.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+
 const Navbar = () => {
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
+
   return (
     <nav className="navbar bg-base-100 shadow-sm">
       <div className="flex-none">
@@ -39,7 +47,8 @@ const Navbar = () => {
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div className="flex-none">
+
+      <div className="flex">
         <label className="swap swap-rotate">
           {/* this hidden checkbox controls the state */}
           <input
@@ -66,6 +75,79 @@ const Navbar = () => {
             <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
+      </div>
+
+      <div className="flex-none">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Theme
+            <svg
+              width="12px"
+              height="12px"
+              className="inline-block h-2 w-2 fill-current opacity-60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+          >
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                data-set-theme="default"
+                aria-label="Default"
+                value="default"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                data-set-theme="retro"
+                aria-label="Retro"
+                value="retro"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                data-set-theme="cyberpunk"
+                aria-label="Cyberpunk"
+                value="cyberpunk"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                data-set-theme="valentine"
+                // data-act-class="ACTIVECLASS"
+                aria-label="Valentine"
+                value="valentine"
+              />
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                data-set-theme="aqua"
+                aria-label="Aqua"
+                value="aqua"
+              />
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
